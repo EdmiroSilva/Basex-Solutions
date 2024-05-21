7a9d276627745769d35b4c7bcfde856227d82db9
Edmiro Silva <130408674+EdmiroSilva@users.noreply.github.com>
2024-05-21 16:46:07 +0100

    Updated Layout

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import { HomeIcon } from "lucide-react";
+import { Play } from "next/font/google";
+import { Navbar } from "@/components/Navbar";
+import { Footer } from "@/components/Footer";
+import { Section } from "@/components/Section";
+import Swiper from "swiper/bundle";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen">
+      <Navbar />
+      <main className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col items-center justify-center">
+          <Section />
+        </div>
+        <Footer />
+      </main>
+    </div>
+  );
+}
